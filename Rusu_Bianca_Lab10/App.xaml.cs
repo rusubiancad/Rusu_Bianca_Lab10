0c5e2e91dd73dbf8d844352e557ab561c0db0fd6
Bianca Denisa Rusu <r.bianca99@yahoo.com>
2020-12-13 23:53:15 +0200

    Add project files.

---
+++ Rusu_Bianca_Lab10/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Rusu_Bianca_Lab10.Data;
+using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Rusu_Bianca_Lab10
+{
+    public partial class App : Application
+    {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+            MainPage = new NavigationPage(new ListEntryPage());
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
